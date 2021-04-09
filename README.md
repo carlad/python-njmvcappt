96ea6724a69e7d4e2d756a33fad1e6236c6631f3
Carla Drago <carla.drago@bsdex.de>
2021-04-09 13:11:07 +0200

    add support for mac and linux systems

--- README.md
+++ README.md
@@ -5,7 +5,16 @@ If you want to search for appointment only on specific locations say Lawrencevil
 location_arr = ['101','104']
 locationname_arr = ['Lawrenceville','Camden']
 
-
-if you want to be notified only when appt is available on specific month, for example only on "April" then update the required moths variable accordingly.
+If you want to be notified only when an appointment is available on specific month, for example only on "April" then update the required months variable accordingly.
 required_months = ['April']
 
+#For Linux Users:#
+Use python3 as you'll run into issues installing/importing dependencies.
+Install BeautifulSoup:
+`pip3 install BeautifulSoup4`
+
+The `lxml` library is required by BeautifulSoup for parsing:
+`pip3 install lxml`
+
+For some reason Ubuntu 20.04 no longer makes a beep noise with `beep`.
+One can use `spd-say` instead, eg `os.system('spd-say %s' % "beep")`. It's a little hacky but it works.
--- dl-alert.py
+++ dl-alert.py
@@ -3,7 +3,20 @@ import re
 import time
 from bs4 import BeautifulSoup
 from datetime import datetime
-import winsound
+
+try:
+    import winsound
+except ImportError:
+    import os
+
+    def playsound(frequency,duration):
+        #apt-get install beep
+        os.system('beep -f %s -l %s' % (frequency,duration))
+        #for those running ubuntu 20.04 uncomment the line below:
+        #os.system('spd-say %s' % "beep")
+else:
+    def playsound(frequency,duration):
+        winsound.Beep(frequency,duration)
 
 location_arr = ['101','102','103','104','105','106','107','108','109','110','111','112','113','114','115','116','117','118','119','120','121','122','123']
 locationname_arr = ['Lawrenceville','Bayonne','North Cape May','Camden','Cardiff','Salem','Delanco','Eatontown','SouthPlainfield','Edison','Flemington','Toms River','Freehold','Lodi','Vineland','Newark','North Bergen','Wayne','Oakland','Paterson','Thorofare','Rahway','Randolph']
@@ -11,21 +24,21 @@ base_url_link='https://telegov.njportal.com/njmvc/AppointmentWizard/11/'
 required_months = ['March','April']
 
 def beep():
-    winsound.Beep(1500, 500)
-    winsound.Beep(4500, 500)
-    winsound.Beep(2500, 500)
-    winsound.Beep(1500, 500)
-    winsound.Beep(4500, 500)
-    winsound.Beep(2500, 500)
+    playsound(1500, 500)
+    playsound(4500, 500)
+    playsound(2500, 500)
+    playsound(1500, 500)
+    playsound(4500, 500)
+    playsound(2500, 500)
 
 
 def job():
+    print("\nChecking appointments")
     now = datetime.now()
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-    print("\n\n\nDate Time: ", dt_string, "\n\n")
+    print("\nDate Time: ", dt_string, "\n")
     i=0
     found=0
-    
     
     for location in location_arr:
         print(location)
@@ -33,13 +46,13 @@ def job():
         soup = BeautifulSoup(page_html.text ,'lxml')
         unavailable=soup.find('div',attrs={'class': 'alert-danger'})
         if unavailable is not None :
-            #print('No appointments are available in '+locationname_arr[i])
+            print('No appointments are available in '+locationname_arr[i])
             dt_string=""
         else:
             dates_html = soup.find('div',attrs={'class': 'col-md-8'})
             date_string = dates_html.find('label',attrs={'class': 'control-label'})
             if set(required_months) & set(date_string.text.split()):
-                #print("Matching required months")
+                print("Matching required months")
                 date_string=re.sub('Time of Appointment for ', '', date_string.text)
                 date_string=re.sub(':', '', date_string)
                 message = 'DL Renew Dates: '+locationname_arr[i]+' / ('+location+') : '+date_string
